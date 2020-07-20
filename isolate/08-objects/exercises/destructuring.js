95d0d6d0adbe9e685428186d64130d9ff53b28aa
Mohammad Musa <mhammad_musa@yahoo.com>
2020-07-20 02:28:17 +0200

    completed destructuring.js

--- isolate/08-objects/exercises/destructuring.js
+++ isolate/08-objects/exercises/destructuring.js
@@ -5,28 +5,28 @@ const expect = chai.expect;
 describe('destructuring', () => {
   it('types of tasty treats', () => {
     const { dry, wet } = {
-      wet: _,
+      wet: 'soup',
       dry: 'bread'
     };
-    expect(obj._).to.eql(dry);
+    expect('bread').to.eql(dry);
     expect(wet).to.eql('soup');
   });
   it('a menagerie', () => {
     const hairiest = 'cheetah';
-    const oceanic = _;
+    const oceanic = 'swimming';
     const obj = {
       [oceanic]: 'mackerel',
       running: hairiest
     };
-    const birdy = _;
-    obj[birdy] = _;
+    const birdy = 'flying';
+    obj[birdy] = 'flying';
     const {
       swimming,
       flying,
       running
     } = obj;
-    expect(flying).to.eql(_);
-    expect(_).to.eql('cheetah');
+    expect(flying).to.eql(birdy);
+    expect(running).to.eql('cheetah');
     expect(swimming).to.eql('mackerel')
   });
 });
